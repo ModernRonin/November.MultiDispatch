ff82aa8fceb4bcb75a5c249400fc7f12bdd731b5
max <max@nix>
2017-01-13 17:01:11 +0100

    more XML comments

--- November.MultiDispatch/DoubleDispatcher.cs
+++ November.MultiDispatch/DoubleDispatcher.cs
@@ -8,6 +8,10 @@ namespace November.MultiDispatch
     {
         readonly Dictionary<Type, Dictionary<Type, CallContext>> mHandlers =
             new Dictionary<Type, Dictionary<Type, CallContext>>();
+        /// <summary>
+        /// This is called if <see cref="Dispatch"/> is called for a combination of argument types for which there
+        /// has been no specific handler defined.
+        /// </summary>
         public Action<TCommonBase, TCommonBase> FallbackHandler { get; set; }
         public void AddHandler(
             Type leftType,
@@ -25,28 +29,61 @@ namespace November.MultiDispatch
                 RightPredicate = rightPredicate
             };
         }
+        /// <summary>
+        /// Fluent definition of the left argument type. Must be followed by <see cref="LeftContinuation{TCommonBase,TLeft}.OnRight{TRight}()"/>
+        /// and <see cref="DoContinuation{TLeft,TRight}.Do"/>.
+        /// </summary>
+        /// <typeparam name="TLeft">the type of the left argument</typeparam>
         public LeftContinuation<TCommonBase, TLeft> OnLeft<TLeft>() where TLeft : TCommonBase
         {
             return new LeftContinuation<TCommonBase, TLeft>(this);
         }
+        /// <summary>
+        /// Fluent definition of the left argument type with an additional check that must be passed for the
+        /// handler to be called.
+        /// Must be followed by <see cref="LeftContinuation{TCommonBase,TLeft}.OnRight{TRight}()"/>
+        /// and <see cref="DoContinuation{TLeft,TRight}.Do"/>.
+        /// </summary>
+        /// <typeparam name="TLeft">the type of the left argument</typeparam>
+        /// <param name="predicate">check that needs to be passed for the handler to be called</param>
         public LeftContinuation<TCommonBase, TLeft> OnLeft<TLeft>(Func<TLeft, bool> predicate) where TLeft : TCommonBase
         {
             return new LeftContinuation<TCommonBase, TLeft>(this, predicate);
         }
+        /// <summary>
+        /// Registers a handler to be called for a certain combination of types.
+        /// </summary>
         public void On<TLeft, TRight>(Action<TLeft, TRight> handler) where TLeft : TCommonBase
             where TRight : TCommonBase
         {
             AddHandler(typeof(TLeft), typeof(TRight), AlwaysTrue<object>(), AlwaysTrue<object>(), handler.ToUntyped());
         }
+        /// <summary>
+        /// Fluent definition of the right argument type.
+        /// Must be followed by <see cref="RightContinuation{TCommonBase,TRight}.OnLeft{TLeft}()"/>
+        /// and <see cref="DoContinuation{TLeft,TRight}.Do"/>.
+        /// </summary>
+        /// <typeparam name="TRight">the type of the right argument</typeparam>
         public RightContinuation<TCommonBase, TRight> OnRight<TRight>() where TRight : TCommonBase
         {
             return new RightContinuation<TCommonBase, TRight>(this);
         }
+        /// <summary>
+        /// Fluent definition of the right argument type with an additional check that must be passed for the
+        /// handler to be called.
+        /// Must be followed by <see cref="RightContinuation{TCommonBase,TRight}.OnLeft{TLeft}()"/>
+        /// and <see cref="DoContinuation{TLeft,TRight}.Do"/>.
+        /// </summary>
+        /// <typeparam name="TRight">the type of the right argument</typeparam>
+        /// <param name="predicate">check that needs to be passed for the handler to be called</param>
         public RightContinuation<TCommonBase, TRight> OnRight<TRight>(Func<TRight, bool> predicate)
             where TRight : TCommonBase
         {
             return new RightContinuation<TCommonBase, TRight>(this, predicate);
         }
+        /// <summary>
+        /// Dispatch a combination of arguments.
+        /// </summary>
         public void Dispatch(TCommonBase left, TCommonBase right)
         {
             var leftType = left.GetType();
