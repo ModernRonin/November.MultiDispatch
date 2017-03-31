b128f3c2f0c3126a698810c0977cc2bae5234f4c
max <max@nix>
2017-03-31 17:39:57 +0200

    got rid of typedistance concept

--- November.MultiDispatch.Tests/TypeExtensionsTests.cs
+++ November.MultiDispatch.Tests/TypeExtensionsTests.cs
@@ -19,54 +19,7 @@ namespace November.MultiDispatch.Tests
 
         interface IThirdInterface { }
         class DerivedImplementation : AnotherImplementation, IThirdInterface {}
-        class Deepest : DerivedImplementation { }
 
-        [Test]
-        public void GetTypeDistanceFromAncestor_To_Unrelated_Type_Returns_IntMax()
-        {
-            typeof(SomeImplementation).GetTypeDistanceFromAncestor(typeof(IDerivedInterface)).Should().Be(int.MaxValue);
-        }
-        [Test]
-        public void GetTypeDistanceFromAncestor_Of_Directly_Derived_Interface_Returns_1()
-        {
-            typeof(IDerivedInterface).GetTypeDistanceFromAncestor(typeof(ISomeInterface)).Should().Be(1);
-        }
-        [Test]
-        public void GetTypeDistanceFromAncestor_To_Base_Class_Returns_1()
-        {
-            typeof(DerivedImplementation).GetTypeDistanceFromAncestor(typeof(AnotherImplementation)).Should().Be(1);
-        }
-        [Test]
-        public void GetTypeDistanceFromAncestor_To_Implemented_Interface_Returns_1()
-        {
-            typeof(DerivedImplementation).GetTypeDistanceFromAncestor(typeof(IThirdInterface)).Should().Be(1);
-        }
-        [Test]
-        public void GetTypeDistanceFromAncestor_To_Grandfather_Class_Returns_2()
-        {
-            typeof(Deepest).GetTypeDistanceFromAncestor(typeof(AnotherImplementation)).Should().Be(2);
-        }
-        [Test]
-        public void GetTypeDistanceFromAncestor_In_Complex_Hierarchy()
-        {
-            var deepest = typeof(Deepest);
-            deepest.GetTypeDistanceFromAncestor(typeof(ISomeInterface)).Should().Be(4);
-        }
-        [Test]
-        public void GetTypeDistanceFromAncestor_Object_And_Object_Returns_0()
-        {
-            typeof(object).GetTypeDistanceFromAncestor(typeof(object)).Should().Be(0);
-        }
-        [Test]
-        public void GetTypeDistanceFromAncestor_String_And_Object_Returns_1()
-        {
-            typeof(string).GetTypeDistanceFromAncestor(typeof(object)).Should().Be(1);
-        }
-        [Test]
-        public void GetTypeDistanceFromAncestor_To_Child_Type_Returns_IntMax()
-        {
-            typeof(object).GetTypeDistanceFromAncestor(typeof(string)).Should().Be(int.MaxValue);
-        }
         [Test]
         public void GetAssignmentTargetTypes_Of_Class()
         {
--- November.MultiDispatch/TypeExtensions.cs
+++ November.MultiDispatch/TypeExtensions.cs
@@ -9,28 +9,12 @@ namespace November.MultiDispatch
     {
         public static IEnumerable<Type> GetAssignmentTargetTypes(this Type self)
         {
-            var parents = self.GetDirectParents();
-            var result = parents.SelectMany(p => p.GetAssignmentTargetTypes()).ToList();
+            var typeInfo = self.GetTypeInfo();
+            var ancestors = typeInfo.ImplementedInterfaces.ToList();
+            if (null != typeInfo.BaseType) ancestors.Add(typeInfo.BaseType);
+            var result = ancestors.Distinct().SelectMany(p => p.GetAssignmentTargetTypes()).ToList();
             result.Add(self);
             return result.Distinct();
-        }
-        public static IEnumerable<Type> GetDirectParents(this Type self)
-        {
-            var typeInfo = self.GetTypeInfo();
-            var parents = typeInfo.ImplementedInterfaces.ToList();
-            if (null != typeInfo.BaseType) parents.Add(typeInfo.BaseType);
-            return parents.Distinct();
-        }
-        public static int GetTypeDistanceFromAncestor(this Type self, Type other)
-        {
-            if (self == other) return 0;
-
-            var parents = self.GetDirectParents().ToArray();
-            if (!parents.Any())
-                return int.MaxValue;
-
-            var minParentDistance = parents.Select(p => p.GetTypeDistanceFromAncestor(other)).Min();
-            return minParentDistance == int.MaxValue ? int.MaxValue : 1 + minParentDistance;
         }
     }
 }
