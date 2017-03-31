1d6f0c26ef3f0d00f81991370ac63d5a4f293cf0
max <max@nix>
2017-03-31 03:34:20 +0200

    added TypeExtensions

---
+++ November.MultiDispatch.Tests/TypeExtensionsTests.cs
@@ -0,0 +1,71 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+
+namespace November.MultiDispatch.Tests
+{
+    [TestFixture]
+    public class TypeExtensionsTests
+    {
+        interface ISomeInterface {}
+
+        interface IDerivedInterface : ISomeInterface, IOtherInterface {}
+
+        interface IOtherInterface {}
+
+        class SomeImplementation : ISomeInterface {}
+
+        class AnotherImplementation : IDerivedInterface {}
+
+        interface IThirdInterface { }
+        class DerivedImplementation : AnotherImplementation, IThirdInterface {}
+
+        [Test]
+        public void GetAssignmentTargetTypes_Of_Class()
+        {
+            typeof(SomeImplementation).GetAssignmentTargetTypes().ShouldAllBeEquivalentTo(new[]
+            {
+                typeof(SomeImplementation),
+                typeof(ISomeInterface),
+                typeof(object)
+            });
+        }
+        [Test]
+        public void GetAssignmentTargetTypes_Of_Derived_Class_With_Complex_Interface_Hierarchy
+            ()
+        {
+            var result = typeof(DerivedImplementation).GetAssignmentTargetTypes();
+            result.ShouldAllBeEquivalentTo(new[]
+            {
+                typeof(DerivedImplementation),
+                typeof(AnotherImplementation),
+                typeof(object),
+                typeof(IThirdInterface),
+                typeof(IDerivedInterface),
+                typeof(IOtherInterface),
+                typeof(ISomeInterface),
+            });
+        }
+        [Test]
+        public void GetAssignmentTargetTypes_Of_Derived_Interface_Returns_Interface_And_Ancestor()
+        {
+            typeof(IDerivedInterface).GetAssignmentTargetTypes().ShouldAllBeEquivalentTo(new[]
+            {
+                typeof(IDerivedInterface), typeof(ISomeInterface), typeof(IOtherInterface)
+            });
+        }
+        [Test]
+        public void GetAssignmentTargetTypes_Of_Interface_Returns_Interface()
+        {
+            typeof(ISomeInterface).GetAssignmentTargetTypes()
+                .Should()
+                .ContainSingle()
+                .Which.Should()
+                .Be<ISomeInterface>();
+        }
+        [Test]
+        public void GetAssignmentTargetTypes_Of_Object_Returns_Object()
+        {
+            typeof(object).GetAssignmentTargetTypes().Should().ContainSingle().Which.Should().Be<object>();
+        }
+    }
+}
---
+++ November.MultiDispatch/TypeExtensions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+namespace November.MultiDispatch
+{
+    public static class TypeExtensions
+    {
+        public static IEnumerable<Type> GetAssignmentTargetTypes(this Type self)
+        {
+            var typeInfo = self.GetTypeInfo();
+
+            var parents = typeInfo.ImplementedInterfaces.ToList();
+            if (null!=typeInfo.BaseType)
+                parents.Add(typeInfo.BaseType);
+
+            var result = parents.SelectMany(p => p.GetAssignmentTargetTypes()).ToList();
+            result.Add(self);
+            return result.Distinct();
+        }
+    }
+}
